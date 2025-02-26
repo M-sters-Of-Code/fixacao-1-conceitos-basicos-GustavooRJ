a47144873429f2600d71b4bb3f2f982c5eee0201
GustavooRJ <gugu.oliveira2012.go@gmail.com>
2025-02-26 20:27:04 -0300

    Creando Classes

---
+++ FixacaoConceitosBasicos1/Model/CalculaAluguelCarro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixacaoConceitosBasicos1.Model
+{
+    internal class CalculaAluguelCarro
+    {
+        public static double Calcular(int dias, int quilometrometragemInicial, int quilometragemFinal)
+        {
+            double valorDiario = 100.0;
+            double valorKm = 0.50;
+
+            int quilometragemRodada = quilometrometragemInicial - quilometragemFinal;
+            double valorTotal = (dias * valorDiario) + (quilometragemRodada * valorKm);
+
+            return valorTotal;
+        }
+}
